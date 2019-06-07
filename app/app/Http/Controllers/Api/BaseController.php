0f8fa2a6506c13b93cd37e4163e3fdcafd985857
Manuel Wutte <mwutte@anexia-it.com>
2019-06-07 14:59:56 +0200

    REST API routes

---
+++ app/app/Http/Controllers/Api/BaseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Class BaseController
+ * @package App\Http\Controllers\Api
+ */
+abstract class BaseController extends Controller {
+
+}
