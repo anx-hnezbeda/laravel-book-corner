8be1d6ca0479aed76aa3c2936283b0e6007b2d83
Manuel Wutte <mwutte@anexia-it.com>
2019-06-19 11:35:53 +0200

    basic schema definition for tags

---
+++ app/app/JsonApi/Tags/Schema.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\JsonApi\Tags;
+
+use Neomerx\JsonApi\Schema\SchemaProvider;
+
+class Schema extends SchemaProvider
+{
+
+    /**
+     * @var string
+     */
+    protected $resourceType = 'tags';
+
+    /**
+     * @param $resource
+     *      the domain record being serialized.
+     * @return string
+     */
+    public function getId($resource)
+    {
+        return (string) $resource->getRouteKey();
+    }
+
+    /**
+     * @param $resource
+     *      the domain record being serialized.
+     * @return array
+     */
+    public function getAttributes($resource)
+    {
+        return [
+            'created-at' => $resource->created_at->toAtomString(),
+            'updated-at' => $resource->updated_at->toAtomString(),
+	        'name' => $resource->name,
+        ];
+    }
+
+	/**
+	 * @param object $resource
+	 * @param bool $isPrimary
+	 * @param array $includeRelationships
+	 *
+	 * @return array
+	 */
+	public function getRelationships( $resource, $isPrimary, array $includeRelationships ) {
+		return [
+			'books' => [
+				self::SHOW_SELF    => true,
+				self::SHOW_RELATED => true,
+				self::SHOW_DATA    => isset( $includeRelationships['books'] ),
+				self::DATA         => function () use ( $resource ) {
+					return $resource->books;
+				}
+			]
+		];
+	}
+}
